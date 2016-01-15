612994754d7bef2abf0dd2428ebf06f349437e18
Max Bilbow <max.bilbow@utilisoft.com>
2016-01-15 15:43:01 +0000

    Seems to work

--- src/main/java/training/casandra/Application.java
+++ src/main/java/training/casandra/Application.java
@@ -1,12 +1,34 @@
 package training.casandra;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.PropertySource;
+
 /**
  * Created by bilbowm (Max Bilbow) on 15/01/2016.
  */
+
+@SpringBootApplication
+@PropertySource({
+        "classpath:application.properties"
+//        ,"file:///${appRootDir}/config/app.properties"
+//        ,"file:///${appRootDir}/lib/version"
+})
 public class Application {
 
     public static void main(String [] args)
     {
+        if (System.getProperty("timestamp") == null)
+            System.setProperty("timestamp","na");
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class);
+        System.out.println("Hello!");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
+        SpringApplication.exit(context,()->0);
     }
 }
