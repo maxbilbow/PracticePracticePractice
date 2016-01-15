cfb0b319d3ddf115bc62502b3d310811df7f2e84
Max Bilbow <max.bilbow@utilisoft.com>
2016-01-15 15:23:42 +0000

    first

---
+++ src/main/java/training/casandra/Application.java
@@ -0,0 +1,12 @@
+package training.casandra;
+
+/**
+ * Created by bilbowm (Max Bilbow) on 15/01/2016.
+ */
+public class Application {
+
+    public static void main(String [] args)
+    {
+
+    }
+}
